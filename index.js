e6f1ebe6e22d39a0641cbb90a20c2e78710f255c
lilly liu <lillyliu@berkeley.edu>
2024-02-25 21:06:22 -0800

    added nodemon

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const port = 8888;
+
 app.get('/', (req, res) => { // get request on index world, send back hello world as a response
-    res.send('Hello World!');
+    const data = {
+        name:  'Lil',
+        isAwesome: true
+    };
+    res.json(data);
 });
 
-const port = 8888;
+app.get('/awesome-generator', (req, res) => {
+    const { name, isAwesome } = req.query;
+    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
+});
+
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
 });
